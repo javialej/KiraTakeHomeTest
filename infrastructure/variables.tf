f5bcb140c5e267fb2b73edd278a11188fde1b2bd
Javier Alejandro Perez Fernandez <ja.perezf11@gmaill.com>
2025-09-13 10:11:39 -0500

    feat: pass environment variables to terraform apply

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,4 +1,3 @@
-
 variable "gcp_project_id" {
   description = "The GCP project ID to deploy to."
   type        = string
@@ -26,4 +25,40 @@ variable "image_tag" {
   description = "The tag for the Docker image."
   type        = string
   default     = "latest"
+}
+
+variable "db_host" {
+  description = "The database host."
+  type        = string
+}
+
+variable "db_port" {
+  description = "The database port."
+  type        = string
+}
+
+variable "db_username" {
+  description = "The database username."
+  type        = string
+}
+
+variable "db_password" {
+  description = "The database password."
+  type        = string
+  sensitive   = true
+}
+
+variable "db_name" {
+  description = "The database name."
+  type        = string
+}
+
+variable "service_name" {
+  description = "The service name."
+  type        = string
+}
+
+variable "collection_name" {
+  description = "The collection name."
+  type        = string
 }
